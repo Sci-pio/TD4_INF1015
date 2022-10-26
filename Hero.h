edbf3ff7ff04683fb9dab3008cf0cfba3acef341
Kamil Maarite <kamil.maarite@polymtl.ca>
2022-10-26 14:40:45 -0400

    messge

--- Hero.h
+++ Hero.h
@@ -2,14 +2,29 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include "Personnage.h"
 
 using namespace std;
 
-class Hero {
+class Hero : public Personnage {
 public:
-	override
+	void virtual changerCouleur(string couleur) const override {
+	if (couleur=="rouge") { cout << "\n\033[90m"; }
+	else if (couleur == "default") { cout << "\n\033[0m"; }
+	}
+
+	void virtual afficher(ostream& os) const override{ 
+		changerCouleur("rouge");
+		os << Ennemi_ << endl;
+		// faut faire os << les trucs du vecteur
+
+		changerCouleur("default");
+	
+	};
 
 private:
-
+	vector<string> listeDesAllies_;
+	string Ennemi_;
 
 };
+
